1a2066856e6f594328d9511045e717dd5525f40e
Sandipan Das <sandipanpyqplatform@gmail.com>
2024-02-26 10:22:16 +0530

    Form data back end added only user data and table added

--- src/Components/Form.js
+++ src/Components/Form.js
@@ -1,16 +1,65 @@
 import React, { useState } from 'react'
-
+import {Table} from 'react-bootstrap'
 export default function Form() {
   let [FormData,setFormData]=useState(
         {
           fname:'',
           lname:'',
-          username:'',
+          uname:'', 
           city:'',
           state:'',
-          zip:''
+          zip:'',
+          index:'', 
         }
     )
+
+    //get value is a function where user can type the value and we can get the value of all the input fields 
+    //on chnage and event are adding for this  here to type
+    let[userData,setuserData]=useState(
+    [
+
+    ]
+
+    )
+    let getValue=(event)=>{
+      let oldData={...FormData}
+      let inputName=event.target.name;   //fname
+      let inputValue=event.target.value; //Sandipan
+      oldData[inputName]=inputValue;      //[fname]=Sandipan
+      // console.log(inputName);         //fname
+      setFormData(oldData)
+      // console.log('Form submitted:', FormData); 
+
+      
+      
+    
+  }
+
+  let handelSubmit=(event)=>{
+    let currentUserFormdata={
+      fname:FormData.fname,
+      lname:FormData.lname,
+      uname:FormData.uname,
+      city:FormData.city,
+      state:FormData.state,
+      zip:FormData.zip,
+    }
+    let oldUserData=[...userData,currentUserFormdata]; 
+    setuserData(oldUserData)
+    // To empty the input field after adding/getting the data
+    setFormData(
+      {
+        fname:'',
+        lname:'',
+        uname:'', 
+        city:'',
+        state:'',
+        zip:'',
+        index:'', 
+      }
+    )
+    event.preventDefault();
+  }
   return (
       
     <div className='container-fluid'>
@@ -23,7 +72,8 @@ export default function Form() {
 
         }}>
         Enquiry form UnIcOdE
-        <i class="fa-regular fa-question"></i>
+        <i class="fa-regular fa-question"
+        ></i>
       </div>
       <div className='box'
         style={{
@@ -31,21 +81,26 @@ export default function Form() {
         }}
       >
         <form className="row g-3"
+        onSubmit={handelSubmit}
+        
           style={{
             marginTop: '25px',
           }}
         >
           <div className="col-md-4">
-            <label htmlFor="validationServer01" class="form-label"
+            <label htmlFor="validationServer01" className="form-label"
             style={{
               fontFamily:"monospace"
             }}>First name</label>
             <input
               type="text"
+              onChange={getValue}
               class="form-control is-valid"
               id="validationServer01"
-              value="Mark"
+              placeholder='Mark'
               name="fname"
+
+              value={FormData.fname}
 
               required />
             <div className="valid-feedback">
@@ -53,24 +108,26 @@ export default function Form() {
             </div>
           </div>
           <div className="col-md-4">
-            <label htmlFor="validationServer02" class="form-label"
+            <label htmlFor="validationServer02" className="form-label"
             style={{
               fontFamily:"monospace"
             }}>
             Last name</label>
             <input
               type="text"
-              class="form-control is-valid"
+              onChange={getValue}
+              className="form-control is-valid"
               id="validationServer02"
-              value="Otto"
+              placeholder='Otto'
               name="lname"
+              value={FormData.lname}
               required />
             <div className="valid-feedback">
               Looks good!
             </div>
           </div>
           <div className="col-md-4">
-            <label htmlFor="validationServerUsername" class="form-label"
+            <label htmlFor="validationServerUsername" className="form-label"
             style={{
               fontFamily:"monospace"
             }}>
@@ -79,10 +136,13 @@ export default function Form() {
               <span className="input-group-text" id="inputGroupPrepend3">@</span>
               <input
                 type="text"
-                class="form-control is-invalid"
+                onChange={getValue}
+                className="form-control is-invalid"
                 id="validationServerUsername"
                 aria-describedby="inputGroupPrepend3 validationServerUsernameFeedback"
                 name='uname'
+                value={FormData.username}
+                placeholder='xyz_123'
                 required />
               <div id="validationServerUsernameFeedback" class="invalid-feedback"
               style={{
@@ -93,16 +153,19 @@ export default function Form() {
             </div>
           </div>
           <div className="col-md-6">
-            <label htmlFor="validationServer03" class="form-label"
+            <label htmlFor="validationServer03" className="form-label"
             style={{
               fontFamily:"monospace"
             }}>City</label>
             <input
               type="text"
-              class="form-control is-invalid"
+              onChange={getValue}
+              className="form-control is-invalid"
               id="validationServer03"
               aria-describedby="validationServer03Feedback"
               name='city'
+              placeholder='California'
+              value={FormData.city}
               required />
             <div id="validationServer03Feedback" class="invalid-feedback"
             style={{
@@ -112,7 +175,7 @@ export default function Form() {
             </div>
           </div>
           <div class="col-md-3">
-            <label htmlFor="validationServer04" class="form-label"
+            <label htmlFor="validationServer04" className="form-label"
             style={{
               fontFamily:"monospace"
             }}>State</label>
@@ -121,6 +184,9 @@ export default function Form() {
               id="validationServer04"
               aria-describedby="validationServer04Feedback"
               name='state'
+              value={FormData.state}
+              placeholder='MarryLand'
+              onChange={getValue}
               required>
               <option selected disabled value="">-----Select your State------</option>
 
@@ -132,23 +198,25 @@ export default function Form() {
               <option>State 6</option>
 
             </select>
-            <div id="validationServer04Feedback" class="invalid-feedback"
+            <div id="validationServer04Feedback" className="invalid-feedback"
             style={{
               fontFamily:"monospace"
             }}>
               Please select a valid state.
             </div>
           </div>
-          <div class="col-md-3">
-            <label htmlFor="validationServer05" class="form-label">Zip</label>
+          <div className="col-md-3">
+            <label htmlFor="validationServer05" className="form-label">Zip</label>
             <input
               type="text"
-              class="form-control is-invalid"
+              onChange={getValue}
+              className="form-control is-invalid"
               id="validationServer05"
               aria-describedby="validationServer05Feedback"
               name='zip'
+              value={FormData.zip}
               required />
-            <div id="validationServer05Feedback" class="invalid-feedback"
+            <div id="validationServer05Feedback" className="invalid-feedback"
             style={{
               fontFamily:"monospace"
             }}>
@@ -178,21 +246,65 @@ export default function Form() {
               </div>
             </div>
           </div>
-          <div class="col-12">
-            <button class="btn btn-primary"
-              type="submit"
+          <div className="col-12">
+          {/*If all the fields are empty then we will show submit button else update button here index are represting the empty and full state if we put index =0/1/or any value in form data then we can see the update button is showing*/}
+          {FormData.index === "" ? (
+  <button className="btn btn-primary" type="submit">Submit</button>
+) : (
+  <span className='btn btn-success' style={{ marginLeft: '5px' }}>Update</span>
+)}
 
-            >Submit form</button>
-            <span className='btn btn-success'
-              style={{
-                marginLeft: '5px'
-              }}> Update </span>
           </div>
         </form>
+        
       </div>
 
+    <div>
+    <Table striped bordered hover>
+  <thead>
+    <tr>
+      <th>#</th>
+      <th>First Name</th>
+      <th>Last Name</th>
+      <th>Username</th>
+      <th>City</th>
+      <th>State</th>
+      <th>Zip</th>
+      <th>Action</th>
+    </tr>
+  </thead>
+  <tbody>
+    {userData.length >= 1 ? (
+      userData.map((obj, i) => (
+        <tr key={i}>
+          <td>{i + 1}</td>
+          <td>{obj.fname}</td>
+          <td>{obj.lname}</td>
+          <td>{obj.uname}</td>
+          <td>{obj.city}</td>
+          <td>{obj.state}</td>
+          <td>{obj.zip}</td>
+          <td>
+            <button className="btn btn-success" type="submit">
+              Update
+            </button>
+            <button className="btn btn-danger" type="submit">
+              Delete
+            </button>
+          </td>
+        </tr>
+      ))
+    ) : (
+      <tr>
+        <td colSpan={8}>No data Found</td>
+      </tr>
+    )}
+  </tbody>
+</Table>
 
     </div>
+    </div>
+    
   )
 }
 
